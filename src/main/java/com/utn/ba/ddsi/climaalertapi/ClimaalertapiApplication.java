6761ef246d5049095178579f2669068848f06ab2
Luc0xUTN <luccrespo@frba.utn.edu.ar>
2026-07-02 13:13:08 -0300

    Inicializacion del proyecto + digrama de clases inicial

---
+++ src/main/java/com/utn/ba/ddsi/climaalertapi/ClimaalertapiApplication.java
@@ -0,0 +1,13 @@
+package com.utn.ba.ddsi.climaalertapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaalertapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaalertapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/ba/ddsi/climaalertapi/ClimaalertapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.ba.ddsi.climaalertapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaalertapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
